f17f8db90523e84dc3e04914338c7958089cba24
crblanaru <crblanaru@gmail.com>
2022-05-25 09:47:16 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # configure git
-git config --global user.name "username"
-git config --global user.email "email address"
+git config --global user.name "crblanaru"
+git config --global user.email "crblanaru@gmail.com"
 
 # install extensions
 /app/code-server/bin/code-server --extensions-dir /config/extensions/ --install-extension rust-lang.rust
